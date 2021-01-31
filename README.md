a7962676eeab6615b21a2c384aac071b3c0c32ac
fukuroda <fukuroder@live.jp>
2021-01-31 21:49:17 +0900

    vst 3.7.1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## bandlimited sawtooth synthesis (vst3/VC++2013)
+## bandlimited sawtooth synthesis (vst3/VC++2019)
 
 ![Screenshot](https://raw.github.com/fukuroder/bandlimited_sawtooth_synthesis/master/screenshot.png)
--- sample/BLITSaw_entry.cpp
+++ sample/BLITSaw_entry.cpp
@@ -8,7 +8,7 @@
 #include "BLITSaw_processor.h"
 #include "BLITSaw_controller.h"
 #include "BLITSaw_guids.h"
-#include "public.sdk/source/main//pluginfactoryvst3.h"
+#include "public.sdk/source/main/pluginfactory.h"
 
 //
 bool InitModule()
@@ -24,35 +24,35 @@ bool DeinitModule()
 	return true;
 }
 
-namespace MyVst{
-	// define factory
-	BEGIN_FACTORY_DEF(
-		"fukuroda",                     // vendor
-		"https://github.com/fukuroder", // url
-		"mailto:fukuroder@live.jp")     // e-mail
+using namespace Steinberg::Vst;
 
-		// register processor
-		DEF_CLASS2(
-		INLINE_UID_FROM_FUID(BLITSawProcessorID), // processor's GUID
-		PClassInfo::kManyInstances,               // ?
-		kVstAudioEffectClass,                     // category
-		"BLITSaw",                                // plug-in name
-		kDistributable,                           // Component flag
-		PlugType::kInstrumentSynth,               // sub category
-		"1.0.0.000",                              // plug-in version
-		kVstVersionString,                        // VSTSDK version
-		BLITSaw_processor::create)                // create method
+// define factory
+BEGIN_FACTORY_DEF(
+	"fukuroda",                     // vendor
+	"https://github.com/fukuroder", // url
+	"mailto:fukuroder@live.jp")     // e-mail
 
-		// register controller
-		DEF_CLASS2(
-		INLINE_UID_FROM_FUID(BLITSawControllerID), // controller's GUID
-		PClassInfo::kManyInstances,                // ?
-		kVstComponentControllerClass,              // category
-		"BLITSaw Controller",                      // plug-in name
-		0,                                         // N/A
-		"",                                        // N/A
-		"1.0.0.000",                               // plug-in version
-		kVstVersionString,                         // create method
-		BLITSaw_controller::create)                // create method
-	END_FACTORY
-} // namespace
+	// register processor
+	DEF_CLASS2(
+	INLINE_UID_FROM_FUID(BLITSawProcessorID), // processor's GUID
+	PClassInfo::kManyInstances,               // ?
+	kVstAudioEffectClass,                     // category
+	"BLITSaw",                                // plug-in name
+	kDistributable,                           // Component flag
+	PlugType::kInstrumentSynth,               // sub category
+	"1.0.0.000",                              // plug-in version
+	kVstVersionString,                        // VSTSDK version
+	MyVst::BLITSaw_processor::create)                // create method
+
+	// register controller
+	DEF_CLASS2(
+	INLINE_UID_FROM_FUID(BLITSawControllerID), // controller's GUID
+	PClassInfo::kManyInstances,                // ?
+	kVstComponentControllerClass,              // category
+	"BLITSaw Controller",                      // plug-in name
+	0,                                         // N/A
+	"",                                        // N/A
+	"1.0.0.000",                               // plug-in version
+	kVstVersionString,                         // create method
+	MyVst::BLITSaw_controller::create)                // create method
+END_FACTORY
